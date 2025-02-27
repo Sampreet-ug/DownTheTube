841a7f3be3bad6861ba98e9c29dd6354d2b01d4f
Sampreet-ug <sampreetgaonkar007@gmail.com>
2025-02-27 11:24:38 +0530

    Updated the code to use yt_dlp instead of pyTube and updated the README.md file accordingly

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-# 'Down The Tube' a YouTube Video Downloader
+# 'Down The Tube' - A YouTube Video Downloader
 
-This is a simple script to download YouTube videos. It uses the `pytube` library to accomplish this.
+This is a simple command-line YouTube video downloader. It supports downloading single and multiple videos using `pytube` and `yt-dlp`.
 
-## Dependencies
+## 📌 Dependencies
 
 - `pytube`
+- `yt-dlp`
+- `ffmpeg`
 - `logging`
 
-You can install these dependencies by running `pip install -r requirements.txt`
+You can install these dependencies by running:
+```sh
+pip install -r requirements.txt
+```
 
-## Usage
+## 🚀 Usage
 
-1. Run the script using `python main.py`
+1. Run the script using:
+```sh
+python src/main.py
+```
 2. You will be presented with a menu:
     ```
     1: Download one video
@@ -22,6 +30,29 @@ You can install these dependencies by running `pip install -r requirements.txt`
 4. If you choose to download multiple videos, you will be asked to enter the save path and then the YouTube links to the videos one by one. Press 'C' to complete the list of links.
 5. The script will then download the videos and save them to the specified path.
 
-## Note
+## 🔗 Additional Features
+- Uses `yt-dlp` for better compatibility with YouTube.
+- Supports downloading the best quality video and audio.
+- Requires `ffmpeg` for merging video and audio streams.
 
-The script downloads the highest resolution MP4 file available for the video. If you wish to change this, you can modify the `downloadSingleVideo` and `downloadMultipleVideo` functions in the script.
+## 💡 Note
+The script downloads the highest resolution MP4 file available for the video by default. If you wish to change this, you can modify the `downloadSingleVideo` and `downloadMultipleVideo` functions in the script.
+
+### Installing FFmpeg
+#### Windows
+- Download from [https://ffmpeg.org/download.html](https://ffmpeg.org/download.html)
+- Add `ffmpeg/bin` to the system PATH.
+
+#### Linux (Ubuntu/Debian)
+```sh
+sudo apt update && sudo apt install ffmpeg -y
+```
+
+#### MacOS
+```sh
+brew install ffmpeg
+```
+
+## 📜 License
+This project is open-source under the MIT License.
+
--- src/DownTheTube.py
+++ src/DownTheTube.py
@@ -1,94 +1,78 @@
-from logging import PercentStyle
 import sys
-import pytube
 from pytube import YouTube
-from pytube.cli import on_progress
+import yt_dlp
 
-def progress_function(self,stream, chunk,file_handle, bytes_remaining):
-
+# Progress callback function
+def progress_function(stream, chunk, bytes_remaining):
     size = stream.filesize
-    p = 0
-    while p <= 100:
-        progress = p
-        print(str(p)+'%')
-        p = PercentStyle(bytes_remaining, size)
+    percent = (size - bytes_remaining) * 100 / size
+    print(f"\rDownloading... {percent:.2f}%", end="")
+
+
 
 def downloadSingleVideo():
-    savePath = input("Enter the save path\n")
-    link = input("Enter the youtube link to video\n")
+    savePath = input("Enter the save path:\n")
+    link = input("Enter the YouTube video link:\n")
 
-    try:     
-        yt = YouTube(link, on_progress_callback=on_progress)
-    except: 
-        print("Connection Error") 
-    
     try:
-        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-        #yt = yt.streams.filter(progressive=True, file_extension='mp4')
-        #print(yt.streams.filter)
-        #res=input("Select the resolution")
-        yt.download(savePath)
-        #stream = yt.streams.get_by_itag(res)
-        #stream.download(savePath)
-        print("(:")
-    except: 
-        print("Some Error!") 
+        ydl_opts = {
+            'outtmpl': f"{savePath}/%(title)s.%(ext)s",
+            'format': 'bestvideo+bestaudio/best',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+        print("\nDownload Complete!")
+    except Exception as e:
+        print(f"Error: {e}")
 
-    print('Task Completed!')
 
 def downloadMultipleVideo():
-
     linkList = []
-    loop = 1
     savePath = input("Enter the save path:\n")
-    
-    while loop >= 1:
-        link = input("Enter the youtube link #" + str(loop) + "\nPress 'C' to complete:\n")
-        if(link != "C"):
-            linkList.append(link)
-            loop+=1
-        else:
-            loop = 0
-            
-    for i in linkList: 
-        try: 
-            yt = YouTube(i, on_progress_callback=on_progress) 
-        except: 
-            print("Connection Error")  
-       
+
+    # Collect video links
+    while True:
+        link = input(f"Enter YouTube link #{len(linkList) + 1} (or press 'C' to complete):\n")
+        if link.upper() == "C":
+            break
+        linkList.append(link)
+
+    # Download videos
+    for index, link in enumerate(linkList, start=1): 
         try:
-            print("Downloading video #"+str(linkList.index(i)+1)+"\n")
-            yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-            yt.download(savePath)
-            print("(:")
-        except:
-            print("Some Error!")
-            
-    print('Task Completed!')
+            ydl_opts = {
+                'outtmpl': f"{savePath}/%(title)s.%(ext)s",
+                'format': 'bestvideo+bestaudio/best',
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([link])
+            print("\nDownload Complete!")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    print('All tasks completed!')
 
 def main():
     print(r"""
-               
-
   _____                        _______ _            _______    _          
  |  __ \                      |__   __| |          |__   __|  | |         
  | |  | | _____      ___ __      | |  | |__   ___     | |_   _| |__   ___ 
  | |  | |/ _ \ \ /\ / / '_ \     | |  | '_ \ / _ \    | | | | | '_ \ / _ \
  | |__| | (_) \ V  V /| | | |    | |  | | | |  __/    | | |_| | |_) |  __/
  |_____/ \___/ \_/\_/ |_| |_|    |_|  |_| |_|\___|    |_|\__,_|_.__/ \___|
-                                                                          
-                                                                          
+    """)
 
- """)
-    val = 0
-    while val == 0:
-        option = input("\nSelect the option:\n1: Download one video\n2: Download multiple videos\n3: Close program\n")
-        if(option == "1"):
+    while True:
+        option = input("\nSelect an option:\n1: Download one video\n2: Download multiple videos\n3: Exit\n")
+        if option == "1":
             downloadSingleVideo()
-        if(option == "2"):
+        elif option == "2":
             downloadMultipleVideo()
-        if(option == "3"):
-            sys.exit("Closing the program")
+        elif option == "3":
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid option. Please enter 1, 2, or 3.")
 
 if __name__ == "__main__":
     main()
